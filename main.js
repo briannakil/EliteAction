d6d5a9e4ac281ba7c5199d6b6c83a80c5ff4ce1c
briannakil <93463051+briannakil@users.noreply.github.com>
2022-02-13 04:19:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector("#navbar-toggleBtn");
+const menu = document.querySelector("#navbar-menu");
+
+
+toggleBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+
+});
